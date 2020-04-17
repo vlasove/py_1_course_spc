6e5c96d7f53420d3c2493d94c6df533d43283722
vlasove <evgeny_vlasov@yahoo.com>
2020-04-17 14:27:03 +0300

    added materials

---
+++ lec13/funcs.py
@@ -0,0 +1,18 @@
+# МОДУЛЬ
+from complexer import minus 
+def add(a,b):
+    return a + b 
+
+def sub(a,b):
+    return minus(a,b)
+
+def mult(a,b):
+    return a * b 
+
+def div(a:int,b:int) -> float:
+    return a / b
+
+
+q = 11
+
+print("HELLO FROM FILE FUNCS.PY")
---
+++ lec13/main.py
@@ -0,0 +1,20 @@
+from funcs import add ,div, q
+from complexer import minus 
+import math 
+
+
+import json #Java Script Object Notation
+import csv 
+
+import collections
+
+math.pi
+
+###code
+c = add(2,3)
+d = div(2,3)
+print(c, d)
+
+print(q)
+####code
+
